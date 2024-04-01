5e00806b0256e651d01e86c4199c16c7ccef6b8f
krylovolegv <krylovolegv@yandex.ru>
2024-04-01 15:58:11 +0200

    add new else 2

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ a = 7 #создание переменной
 b = 9
 
 if a > b:
-    print("a more than b")
+    print("a bolshe b")
 elif b < a:
-    print("b more than a")
+    print("b menshe a")
+else:
+    print("a ravno b")
