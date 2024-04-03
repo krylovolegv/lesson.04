41287ab8c667e0be505ada451af2d8d94fd6ed8a
krylovolegv <krylovolegv@yandex.ru>
2024-04-03 12:31:05 +0200

    new code1

--- main.py
+++ main.py
@@ -1,12 +1,2 @@
-print ("я конфликтую")
-for i in range(8):
-    print("slovo")
-a,b = 9,8
-
-if a > b:
-    print("a bolshe b")
-elif b < a:
-    print("b menshe a")
-else:
-    print("a ravno b")
+a = 5
 
