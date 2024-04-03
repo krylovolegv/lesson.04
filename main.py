4a07d0aa217458cddf11f2094d772e49586b983a
krylovolegv <krylovolegv@yandex.ru>
2024-04-03 12:03:24 +0200

    new code print in main

--- main.py
+++ main.py
@@ -1,11 +1,1 @@
-a = 7 #создание переменной
-b = 9
-
-if a > b:
-    print("a bolshe b")
-elif b < a:
-    print("b menshe a")
-else:
-    print("a ravno b")
-
-print("dontlookatme")
+print ("я конфликтую")
